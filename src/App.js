3f60108aee54c0042e017fd911688f215c3bf7d2
Veikko Hiltunen <veikko.hiltun@gmail.com>
2021-01-15 18:06:41 +0200

    Basic query works

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+
 import React from 'react';
+import { query } from './api/query';
 
 const App = () => {
-  return <div></div>;
+	query();
+	return (
+		<Router>
+			<Switch>
+				<Route path='/' />
+				<Route path='/series' />
+				<Route path='/movies' />
+			</Switch>
+		</Router>
+	);
 };
 
 export default App;
---
+++ src/api/query.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+export const query = () => {
+	const params = {
+		api_key: `api_key=${process.env.REACT_APP_API_KEY}`,
+		entertainment: 'movie', // movie, tv
+		type: 'popular', // popular, latest, now_playing, upcoming, top_rated
+		base_uri: process.env.REACT_APP_BASE_URI,
+		language: 'language=en-US',
+		page: 1,
+	};
+
+	const generateQuery = async () => {
+		const query = `${params.base_uri}/${params.entertainment}/${params.type}?${params.api_key}&${params.language}&page=${params.page}`;
+
+		try {
+			const result = await axios.get(query);
+			console.log(result);
+		} catch (e) {
+			console.error(e);
+		}
+	};
+	// generateQuery();
+};
