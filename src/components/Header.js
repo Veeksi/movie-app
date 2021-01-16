e8736d09f9a8e94524a86f91933529bf9be8b0f9
Veikko Hiltunen <veikko.hiltun@gmail.com>
2021-01-16 02:11:59 +0200

    Search bar added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,11 @@ import { Link } from 'react-router-dom';
 import React from 'react';
 
 const Header = () => {
+	const handleSearch = (e) => {
+		if (e.key === 'Enter') {
+			console.log(e.target.value);
+		}
+	};
 	return (
 		<div className='header-2'>
 			<nav className='bg-white py-2 md:py-5'>
@@ -10,6 +15,15 @@ const Header = () => {
 						className='hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0'
 						id='navbar-collapse'
 					>
+						<div className='relative mx-auto text-gray-600 lg:block hidden'>
+							<input
+								className='border-2 border-gray-300 bg-white h-10 pl-2 pr-8 rounded-lg text-sm focus:outline-none'
+								type='search'
+								name='search'
+								placeholder='Search'
+								onKeyDown={handleSearch}
+							/>
+						</div>
 						<Link
 							to='/movies'
 							className='p-2 lg:px-4 md:mx-2 text-gray-600 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300'
