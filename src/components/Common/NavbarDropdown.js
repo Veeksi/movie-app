4ddbc189b20e72913f6611e5a837102494ef8e41
Veikko Hiltunen <veikko.hiltun@gmail.com>
2021-01-19 17:21:14 +0200

    Fix navbar dropdown options

--- src/components/Common/NavbarDropdown.js
+++ src/components/Common/NavbarDropdown.js
@@ -1,12 +1,51 @@
 import React, { useRef } from 'react';
+import { keys, lowerCase } from 'lodash';
 
 import { Link } from 'react-router-dom';
-import { lowerCase } from 'lodash';
 import { useDetectOutsideClick } from '../../hooks/useDetectOutsideClick';
 
 export const NavbarDropdown = ({ title }) => {
   const dropDownRef = useRef(null);
   const [isActive, setIsActive] = useDetectOutsideClick(dropDownRef, false);
+
+  const searchQuery = {
+    movies: {
+      popular: {
+        label: 'Popular',
+        type: 'popular',
+      },
+      top_rated: {
+        label: 'Top rated',
+        type: 'top_rated',
+      },
+      now_playing: {
+        label: 'Now playing',
+        type: 'now_playing',
+      },
+      upcoming: {
+        label: 'Upcoming',
+        type: 'upcoming',
+      },
+    },
+    tv: {
+      popular: {
+        label: 'Popular',
+        type: 'popular',
+      },
+      top_rated: {
+        label: 'Top rated',
+        type: 'top_rated',
+      },
+      airing_today: {
+        label: 'Airing today',
+        type: 'airing_today',
+      },
+      on_the_air: {
+        label: 'On TV',
+        type: 'on_the_air',
+      },
+    },
+  };
 
   return (
     <div className="relative group" ref={dropDownRef}>
@@ -25,26 +64,33 @@ export const NavbarDropdown = ({ title }) => {
             : 'hidden'
         }
       >
-        <Link to={`/${lowerCase(title)}/popular`}>
-          <p className="px-4 py-2 text-gray-800 hover:bg-gray-700 hover:text-white">
-            Popular
-          </p>
-        </Link>
-        <Link to={`/${lowerCase(title)}/top_rated`}>
-          <p className="px-4 py-2 text-gray-800 hover:bg-gray-700 hover:text-white">
-            Top rated
-          </p>
-        </Link>
-        <Link to={`/${lowerCase(title)}/now_playing`}>
-          <p className="px-4 py-2 text-gray-800 hover:bg-gray-700 hover:text-white">
-            Now playing
-          </p>
-        </Link>
-        <Link to={`/${lowerCase(title)}/upcoming`}>
-          <p className="px-4 py-2 text-gray-800 hover:bg-gray-700 hover:text-white">
-            Upcoming
-          </p>
-        </Link>
+        {title === 'Movies' ? (
+          <div>
+            {Object.keys(searchQuery.movies).map((item, idx) => (
+              <Link
+                key={idx}
+                to={`/${lowerCase(title)}/${searchQuery.movies[item].type}`}
+              >
+                <p className="px-4 py-2 text-gray-800 hover:bg-gray-700 hover:text-white">
+                  {searchQuery.movies[item].label}
+                </p>
+              </Link>
+            ))}
+          </div>
+        ) : (
+          <div>
+            {Object.keys(searchQuery.tv).map((item, idx) => (
+              <Link
+                key={idx}
+                to={`/${lowerCase(title)}/${searchQuery.tv[item].type}`}
+              >
+                <p className="px-4 py-2 text-gray-800 hover:bg-gray-700 hover:text-white">
+                  {searchQuery.tv[item].label}
+                </p>
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
