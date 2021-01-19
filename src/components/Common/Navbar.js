f615c289eda1fa3bdd1b23e2553d889c6c0454b9
Veikko Hiltunen <veikko.hiltun@gmail.com>
2021-01-19 16:34:48 +0200

    Add home route within home padge

--- src/components/Common/Navbar.js
+++ src/components/Common/Navbar.js
@@ -27,18 +27,20 @@ const Navbar = (props) => {
 
   return (
     <nav className="flex items-center flex-wrap bg-black p-3">
-      <p className="inline-flex items-center p-2 mr-4 ">
-        <svg
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          className="fill-current text-white h-8 w-8 mr-2"
-        >
-          <path d="M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z" />
-        </svg>
-        <span className="text-xl text-white font-bold uppercase tracking-wide">
-          THE MOVIE APP
-        </span>
-      </p>
+      <Link to="/">
+        <p className="inline-flex items-center p-2 mr-4 ">
+          <svg
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            className="fill-current text-white h-8 w-8 mr-2"
+          >
+            <path d="M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z" />
+          </svg>
+          <span className="text-xl text-white font-bold uppercase tracking-wide">
+            THE MOVIE APP
+          </span>
+        </p>
+      </Link>
       <button
         className=" inline-flex p-3 hover:bg-gray-700 rounded lg:hidden text-white ml-auto hover:text-white focus:outline-none"
         onClick={handleClick}
@@ -72,7 +74,10 @@ const Navbar = (props) => {
           <NavbarDropdown title="Movies" />
           <NavbarDropdown title="Series" />
 
-          <form onSubmit={handleSearch} className="flex flex-row lg:pt-0 pt-1 text-base">
+          <form
+            onSubmit={handleSearch}
+            className="flex flex-row lg:pt-0 pt-1 text-base"
+          >
             <select
               className="pl-3 shadow  outline-none focus:shadow-outline pr-3 rounded-l bg-gray-100"
               onChange={(e) => setType(e.target.value)}
