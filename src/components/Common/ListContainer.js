0bccfa720d345b8d72b811a25960e8312cc6512c
Veikko Hiltunen <veikko.hiltun@gmail.com>
2021-01-20 16:30:03 +0200

    Fix card linking

--- src/components/Common/ListContainer.js
+++ src/components/Common/ListContainer.js
@@ -15,47 +15,37 @@ const ListContainer = ({ data }) => {
                 className="flex flex-col w-1/2 sm:w-1/4 md:w-1/5 lg:w-1/6 text-primary bg-card hover:bg-hover hover:text-hover"
                 key={idx}
               >
-                <article className="flex flex-col flex-grow p-2 overflow-hidden rounded shadow-lg h-full">
-                  <Link to={`/${item.media_type}/${item.id}`}>
+                <Link to={`/${item.media_type}/${item.id}`}>
+                  <article className="flex flex-col flex-grow p-2 overflow-hidden rounded shadow-lg h-full">
                     <img
                       alt="Placeholder"
                       className="block h-auto w-full"
                       src={`${process.env.REACT_APP_IMAGE_URI}${item.poster_path}`}
                     />
-                  </Link>
 
-                  <header className="flex flex-grow leading-tight p-2 md:p-4">
-                    <h1 className="text-lg">
-                      <Link
-                        to={`/${item.media_type}/${item.id}`}
-                        className="no-underline hover:underline block"
-                      >
+                    <header className="flex flex-grow leading-tight p-2 md:p-4">
+                      <h1 className="text-lg">
                         {item.original_title ? item.original_title : item.name}
-                      </Link>
-                    </h1>
-                  </header>
+                      </h1>
+                    </header>
 
-                  <main className="flex items-center justify-between leading-none p-2 md:p-4">
-                    <p className="text-sm">
-                      {item.release_date
-                        ? item.release_date
-                        : item.first_air_date}
-                    </p>
-                    <p className="text-sm">{`${item.vote_average}☆`}</p>
-                  </main>
+                    <main className="flex items-center justify-between leading-none p-2 md:p-4">
+                      <p className="text-sm">
+                        {item.release_date
+                          ? item.release_date
+                          : item.first_air_date}
+                      </p>
+                      <p className="text-sm">{`${item.vote_average}☆`}</p>
+                    </main>
 
-                  <footer className="flex justify-between leading-none p-2 md:p-4">
-                    <Link
-                      to={`/${item.media_type}/${item.id}`}
-                      className="no-underline hover:underline"
-                    >
+                    <footer className="flex justify-between leading-none p-2 md:p-4">
                       <p className="text-sm">
                         {upperCase(item.original_language)}
                       </p>
-                    </Link>
-                    <p className="text-sm">{capitalize(item.media_type)}</p>
-                  </footer>
-                </article>
+                      <p className="text-sm">{capitalize(item.media_type)}</p>
+                    </footer>
+                  </article>
+                </Link>
               </div>
             )
         )}
