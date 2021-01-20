37d47527371e514002dd4f1183e00f6b13773ac9
Veikko Hiltunen <veikko.hiltun@gmail.com>
2021-01-20 22:12:13 +0200

    Add scrollbar item hovering

--- src/components/Movies/SimilarMovies.js
+++ src/components/Movies/SimilarMovies.js
@@ -6,11 +6,11 @@ const SimilarMovies = ({ data }) => {
   return (
     <div className="flex flex-nowrap overflow-x-auto mx-2 scrollbar scrollbar-thumb-gray-500 dark:scrollbar-thumb-gray-900 scrollbar-track-white dark:scrollbar-track-white">
       {data.results.map((result, idx) => (
-        <div className="flex-none mb-4" key={idx}>
+        <div className="flex-none mb-5" key={idx}>
           <Link to={`/movie/${result.id}`}>
             <img
               alt="Placeholder"
-              className="block"
+              className="block rounded hover:border-hover border-4"
               src={`${process.env.REACT_APP_IMAGE_URI}${result.poster_path}`}
             />
           </Link>
