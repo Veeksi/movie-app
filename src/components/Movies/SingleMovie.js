affd7590e48cc5633e2c295e1f13c5f39dcd90ea
Veikko Hiltunen <veikko.hiltun@gmail.com>
2021-01-20 18:23:31 +0200

    Add basic single movie view

--- src/components/Movies/SingleMovie.js
+++ src/components/Movies/SingleMovie.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
+import { convertMinsToTime } from '../../utils/getRuntime';
 import { getSingleMovie } from '../../api/movies';
 import { useParams } from 'react-router-dom';
 
@@ -33,18 +34,42 @@ const SingleMovie = () => {
   }
   console.log(data);
   return (
-    <div className="flex flex-col flex-grow">
-      <div className="flex container mx-auto px-4 lg:px-0 py-3 justify-between align-middle">
-        <div className="flex flex-row container mx-auto my-2 p-2 md:px-2 lg:p-2 bg-primary pb-1">
-          <img
-            alt="Placeholder"
-            className="block"
-            src={`${process.env.REACT_APP_IMAGE_URI}${data.poster_path}`}
-          />
+    <div className="flex flex-col flex-grow text-primary bg-secondary">
+      <div className="flex container mx-auto px-2 lg:px-0">
+        <div className="flex flex-row flex-wrap container mx-auto my-2 p-2 md:px-2 lg:p-2 bg-primary pb-1">
+          <div className="flex flex-col">
+            <img
+              alt="Placeholder"
+              className="block"
+              src={`${process.env.REACT_APP_IMAGE_URI}${data.poster_path}`}
+            />
+            <p className="italic">{data.tagline}</p>
+          </div>
 
-          <h1 className="text-3xl font-bold px-2">
-            {data.original_title ? data.original_title : data.name}
-          </h1>
+          <div className="flex flex-col sm:px-2 md:px-2 lg:px-2">
+            <h1 className="text-3xl font-bold">
+              {data.original_title ? data.original_title : data.name}
+            </h1>
+            <p className="text-base">Relase date: {data.release_date}</p>
+
+            <div className="flex flex-row flex-wrap">
+              {data.genres.map((genre, idx) => (
+                <p
+                  key={idx}
+                  className="max-w-max p-1 mr-2 my-1 bg-card rounded"
+                >
+                  {genre.name}
+                </p>
+              ))}
+            </div>
+
+            <p className="text-base">{convertMinsToTime(data.runtime)}</p>
+            <p className="text-base">Status: {data.status}</p>
+          </div>
+
+          <div className="flex flex-col flex-wrap container mx-auto my-1 bg-primary">
+            <p className="text-base">{data.overview}</p>
+          </div>
         </div>
       </div>
     </div>
